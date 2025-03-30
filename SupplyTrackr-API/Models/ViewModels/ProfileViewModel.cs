bc1fe892a05d5534638b061752ffd04dc43ea6a2
Reese Elijah Moralde <relijahmoralde@gmail.com>
2025-03-30 16:14:50 +0800

    git commit

---
+++ SupplyTrackr-API/Models/ViewModels/ProfileViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupplyTrackr_Profile.Models.ViewModels
+{
+    public class ProfileViewModel
+    {
+        [Key]
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string PasswordHash { get; set; }
+        public required string FirstName { get; set; }
+        public required string MiddleName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public required string Role { get; set; }
+
+        public required string Account { get; set; }
+
+        public DateTime CreatedDate { get; set; }
+        public DateTime LastLoginDate { get; set; }
+    }
+}
+
+
