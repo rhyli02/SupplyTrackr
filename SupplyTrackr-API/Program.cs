d5f6be8e2711447df41c33d9f24b709adaca518d
JCG0914 <136719007+JCG0914@users.noreply.github.com>
2025-04-06 07:30:22 +0800

    commit

--- SupplyTrackr-API/Program.cs
+++ SupplyTrackr-API/Program.cs
@@ -8,6 +8,17 @@ using SupplyTrackr_API.Services.Interface;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//CORS Policy
+
+builder.Services.AddCors(options => {
+    options.AddPolicy("CorsPolicy",
+        builder => builder
+                    .WithOrigins("https://localhost:5173")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+});
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -58,6 +69,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
